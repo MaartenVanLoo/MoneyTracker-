398b2496632c5b8e411e5b63e4541c15c02cbd9d
Leander <leander.vdheuvel@gmail.com>
2021-12-16 15:50:08 +0100

    utest al wat geschreven
    now wat werk om de tests te laten werken

--- src/test/java/IteratorTest.java
+++ src/test/java/IteratorTest.java
@@ -1,5 +1,6 @@
 
 import database.TicketIterator;
+import org.javamoney.moneta.Money;
 import tickets.*;
 import org.junit.Assert;
 import org.junit.runner.RunWith;
@@ -13,6 +14,10 @@ import org.powermock.api.mockito.PowerMockito;
 
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
+
+import javax.money.MonetaryAmount;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 @RunWith(PowerMockRunner.class)
 // Prepare class RegistrationController for testing by injecting mocks
@@ -35,14 +40,28 @@ public class IteratorTest {
         MemberDatabase mock_MDB = Mockito.mock(MemberDatabase.class);
         EvenTicket mock_ET = Mockito.mock(EvenTicket.class);
         UnevenTicket mock_UT = Mockito.mock(UnevenTicket.class);
+
+        // mock constructors
+        HashMap<String , MonetaryAmount> mock_Hash = (HashMap<String, MonetaryAmount>)Mockito.mock(HashMap.class);
+        ArrayList<String> test_debtors = (ArrayList<String>)Mockito.mock(ArrayList.class);
+        test_debtors.add("a");
+        test_debtors.add("b");
+        test_debtors.add("c");
+        ArrayList<MonetaryAmount> test_debts = (ArrayList<MonetaryAmount>)Mockito.mock(ArrayList.class);
+        test_debts.add(Money.of(10,"EUR"));
+        test_debts.add(Money.of(20,"EUR"));
+        test_debts.add(Money.of(30,"EUR"));
+        mock_MDB.addEntry("a",mock_Hash);
+        Ticket testtick = new Ticket("a", EventTickets.AirplaneTicket, Money.of(10,"EUR"),test_debtors, test_debts );
+        mock_TDB.addEntry(1,testtick);
         MemberDatabase mock_mdb = MemberDatabase.getInstance();
         TicketDatabase mock_tdb = TicketDatabase.getInstance();
 
-        // mock constructors
 
         TicketIterator iteratorUnderTest = new TicketIterator(mock_tdb);
-        iteratorUnderTest.end();
-        iteratorUnderTest.first();
+        Assert.assertEquals(testtick, iteratorUnderTest.first());
+        Assert.assertEquals(false ,iteratorUnderTest.end());
+
         iteratorUnderTest.next();
         iteratorUnderTest.prev();
 
